9d9e6ef6b2eac19bad4108881010641934439d5a
b08Walls <octavioroberto@hotmail.com>
2018-03-01 16:37:13 -0600

    upload
    updload
    
    Co-Authored-By: b08walls <b08walls@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,95 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var mqtt = require('mqtt');
+
+var options = {
+    port: 10246,
+    host: 'm14.cloudmqtt.com',
+    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+    username: 'xxjpxcmq',
+    password: new Buffer('Bp48RyeHOmBc'),
+    keepalive: 60,
+    reconnectPeriod: 1000,
+    protocolId: 'MQIsdp',
+    protocolVersion: 3,
+    clean: true,
+    encoding: 'utf8'
+};
+
+console.log("INTENTANDO CONECTAR");
+//var client = mqtt.connect('http://m14.cloudmqtt.com',options);
+var client = mqtt.connect('http://192.168.0.65');
+//console.log(client);
+
+var io = require('socket.io')(http);
+
+var socketOn = false;
+
+client.on('connect',function(){
+	client.subscribe('presence');
+	client.publish('presence','Hello mqtt');
+	socketOn = true;
+	alert("CONECTADO MQTT");
+});
+
+client.on('message',function(topic,message){
+	console.log(message.toString());
+	if(socketOn)
+	{
+		io.emit('chat message',message.toString());
+	}
+});
+
+client.on('error', function(err) {
+    console.log(err);
+});
+
+app.get('/',function(req,res)
+{
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/jquery.js',function(req,res)
+{
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname+'/node_modules/jquery/dist/jquery.js');
+});
+
+app.get('/d3.js',function(req,res)
+{
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname+'/d3.v3.min.js');
+});
+
+app.get('/script.js',function(req,res)
+{
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname+'/script.js');
+});
+
+app.use(express.static(__dirname+'/public'))
+
+io.on('connection',function(socket)
+{
+	console.log('a user connected');
+	io.on('connection',function(socket){
+		socket.broadcast.emit("hi");
+	});
+
+	socket.on('disconnect',function(){
+		console.log('user disconnected')
+	});
+
+	socket.on('chat message',function(msg){
+		console.log('message: '+msg);
+		io.emit('chat message',msg);
+		socketOn = true;
+	});
+});
+
+http.listen(3000,function()
+{
+	console.log('listening on *:3000');
+});
---
+++ index2.js
@@ -0,0 +1,116 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var Paho = require('paho-mqtt')
+
+//******************************************************************************************
+//***CONFIGURACION Y USO DE CODIGO MQTT PARA SERVIDOR EN NODE*******************************
+//******************************************************************************************
+
+var location = {
+				hostname: "m14.cloudmqtt.com",
+				port: "10246"
+}
+
+// Create a client instance
+client = new Paho.MQTT.Client(location.hostname, Number(location.port), "clientId");
+ 
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+ 
+// connect the client
+client.connect({onSuccess:onConnect});
+ 
+ 
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("onConnect");
+  client.subscribe("World");
+  message = new Paho.MQTT.Message("Hello");
+  message.destinationName = "World";
+  client.send(message);
+}
+ 
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+ 
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+}
+
+//******************************************************************************************
+
+
+var socketOn = false;
+
+client.on('connect',function(){
+	client.subscribe('presence');
+	client.publish('presence','Hello mqtt');
+	socketOn = true;
+	alert("CONECTADO MQTT");
+});
+
+client.on('message',function(topic,message){
+	console.log(message.toString());
+	if(socketOn)
+	{
+		io.emit('chat message',message.toString());
+	}
+	// client.end();
+});
+
+app.get('/',function(req,res)
+{
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/jquery.js',function(req,res)
+{
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname+'/node_modules/jquery/dist/jquery.js');
+});
+
+app.get('/d3.js',function(req,res)
+{
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname+'/d3.v3.min.js');
+});
+
+app.get('/script.js',function(req,res)
+{
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname+'/script.js');
+});
+
+app.use(express.static(__dirname+'/public'))
+
+io.on('connection',function(socket)
+{
+	console.log('a user connected');
+	io.on('connection',function(socket){
+		socket.broadcast.emit("hi");
+	});
+
+	socket.on('disconnect',function(){
+		console.log('user disconnected')
+	});
+
+	socket.on('chat message',function(msg){
+		console.log('message: '+msg);
+		io.emit('chat message',msg);
+		socketOn = true;
+	});
+});
+
+http.listen(3000,function()
+{
+	console.log('listening on *:3000');
+});
