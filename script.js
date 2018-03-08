0f6c83f57fbda99d7dca5e0fc4926ac6fa0bbc74
b08Walls <octavioroberto@hotmail.com>
2018-03-08 12:32:49 -0600

    MapReady

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 $(function() {
 
-
+  /*En esta funcion se declaran los diferentes bloques que conforman el layout de la planta y luego son retornados*/
   function getBlocks()
   {
     bloques = [];
 
     bloques.push(createBlock(0,0,14,34))
-    //PSG
     bloques.push(createBlock(17,3,31,6,"psg k2.jpg"))
     bloques.push(createBlock(17,12,31,22,"psg linea princial.jpg"))
     bloques.push(createBlock((51),3,5,31,"PSG FIFO.jpg"))
@@ -14,7 +13,6 @@ $(function() {
     bloques.push(createBlock(0,0,5,70,"almacenes.jpg"))
     bloques.push(createBlock((8),37,6,28))
     bloques.push(createBlock(44,37,45,17,"war room.jpg"))
-    //bloques.push(createBlock(80,37,9,18))
     bloques.push(createBlock(17,37,24,8,"actuador.jpg"))
     bloques.push(createBlock(17,47,24,8,"discos 3.jpg"))
     bloques.push(createBlock(17,58,24,7,"discos 2.jpg"))
@@ -25,19 +23,26 @@ $(function() {
     return bloques;
   }
 
+  /*Esta funcion juega el papel del constructor de un objeto bloque*/
   function createBlock(x,y,width,height,path)
   {
+    //Se inicializa el objeto
     var block = {};
+    /*Comienzan a agregarse propiedades, ubicacion con coordenadas X y Y*/
     block.x = Math.floor(x);
     block.y = Math.floor(y);
+    /*Altura y anchura*/
     block.height = Math.floor(height);
     block.width = Math.floor(width);
 
+    /*Se comiuenzan a describir ciertas propiedades de la imagen que corresponde a 
+    este bloque, que son: anchura, altura, ubicacion en pixeles en x y ubicacion en pixeles en y*/
     block.imageWidth = (block.width+1)*squareLength;
     block.imageHeight = (block.height+1)*squareLength;
     block.imageX = (block.x*squareLength)+5;
     block.imageY = (block.y*squareLength)+5;
 
+    /*Se corrobora la existencia del path*/
     if(path!== undefined)
     {
       block.imagePath = path;
@@ -47,6 +52,7 @@ $(function() {
       block.imagePath = undefined;
     }
 
+    //Se retorna el objeto al usuario.
     return block;
   }
 
@@ -63,67 +69,11 @@ $(function() {
     return x==0 || y == 0 || x == (gridSize.x-1) || y == (gridSize.y-1);
   }
 
-  /**/
-  function buildMap(gridSize, ratios) {
-    /*map es un objeto que contiene cuatro arreglos los cuales guardan los valores 
-    de la cuadricula en general, los cuadros que son pasto, los cuadros que son piedra
-    y los cuadros que son lava*/
-    var map = { grid:[], grass:[], rock:[], lava:[] };
-
-    /*Este for recorre todos los valores del eje x de la cuadricula uno por uno*/
-    for (x = 0; x < gridSize.x; x++) {
-        /*crea en cada instancia del objeto map, en su atributo "grid" un arreglo vacio, haciendo entonces
-        al atributo grid un arreglo bidimencional*/
-        map.grid[x] = [];
-        /*De forma anidada otro for recorre cada casilla del eje y de la cuadricula casilla por casilla(como ya sabes quien)*/
-        for (y = 0; y < gridSize.y; y++) {
-            /*Dentro de la variable local rock se guarda un valor boleano que depende de un numero random generado de
-            0 a 1 el cual debera ser menor que el ratio indicado al momento de llamar al metodo para poder ser true
-            y sera false si es mayor que dicho ratio*/
-            var rock = Math.random() < ratios.rock;
-            /*Dentro de la variable local lava se guarda otro booleano con la misma metodologia de la variable rock*/
-            var lava = Math.random() < ratios.lava;
-            /*Dentro de la variable type sera igual a rock si es un borde, caso contrario sera grass*/
-            var type = isBorder(x, y, gridSize)?"rock":"grass";
-            /*Si no era borde y se le asigno tipo grass pero la posibilidad de rock fue true, entonces se asignara tipo rock*/
-            if(rock) {
-              type = "rock";
-            }
-            /*Aun cuando no haya sido borde, haya sido rock, si fue tipo lava, este sera el tipo asignado*/
-            if(lava) {
-              type = "lava";
-            }
-            /*En la variable cel, se guardara un objeto el cual tiene 3 atributos llamados x, y y type a los cuales se les asignan
-            los valores de sus respectivas variables*/
-            var cell = { x:x, y:y , type:type };
-            /*dentro del objeto map en el atributo grid que es un arreglo bidimencional se guardara el objeto cell correspondiente*/
-            map.grid[x][y] = cell;
-            /*De acuerdo al tipo de la celda, se guardara en el atributo correspondiente dentro del objeto map la celda por medio 
-            de la llamada al metodo push*/
-            map[type].push(cell);
-        }
-    }
-    /*Al finalizar la insercion y generacion de todas las celdas se regresara el objeto mapa*/
-    return map;
-    //******************************************************************************************************
-    //***CONCLUSION*****************************************************************************************
-    //******************************************************************************************************
-    /*El objeto retornado map, contiene 4 atributos, todos se llenan con objetos cell, los objetos cell estan
-    conformados por medio de coordenadas "x" y "y" asi como el tipo correspondiente a la celda
-
-    Los 4 atributos que tiene el objeto retornado son:
-      - grid: este arreglo 2D guarda todas las celdas de manera ordenada con sus coordenadas y su tipo
-      - grass: este arreglo 1D guarda todas las celdas tipo grass
-      - rock: este arreglo 1D guarda todas las celdas tipo rock
-      - lava: este arreglo 1D guarda todas las celdas tipo lava*/
-
-  }
-
   function buildMap2(gridSize, ratios) {
     /*map es un objeto que contiene cuatro arreglos los cuales guardan los valores 
     de la cuadricula en general, los cuadros que son pasto, los cuadros que son piedra
-    y los cuadros que son lava*/
-    var map = { grid:[], grass:[], rock:[], lava:[] };
+    y los cuadros que son border*/
+    var map = { grid:[], grass:[], rock:[], border:[] };
 
     /*Este for recorre todos los valores del eje x de la cuadricula uno por uno*/
     for (x = 0; x < gridSize.x; x++) {
@@ -136,10 +86,10 @@ $(function() {
             0 a 1 el cual debera ser menor que el ratio indicado al momento de llamar al metodo para poder ser true
             y sera false si es mayor que dicho ratio*/
             //var rock = Math.random() < ratios.rock;
-            /*Dentro de la variable local lava se guarda otro booleano con la misma metodologia de la variable rock
-            var lava = Math.random() < ratios.lava;*/
+            /*Dentro de la variable local border se guarda otro booleano con la misma metodologia de la variable rock
+            var border = Math.random() < ratios.border;*/
             /*Dentro de la variable type sera igual a rock si es un borde, caso contrario sera grass*/
-            var type = isBorder(x, y, gridSize)?"rock":"grass";
+            var type = isBorder(x, y, gridSize)?"border":"grass";
             
             /*En la variable cel, se guardara un objeto el cual tiene 3 atributos llamados x, y y type a los cuales se les asignan
             los valores de sus respectivas variables*/
@@ -150,12 +100,8 @@ $(function() {
             {
               if(x>bloques[i].x && x<=bloques[i].x+bloques[i].width && y>bloques[i].y && y<=bloques[i].y+bloques[i].height)
               {
-                //console.log("x: "+x+", y: "+y);
+
                 type = "rock";
-                // if(bloques[i].imagePath!== undefined)
-                // {
-                //   console.log(bloques[i].x,x,bloques[i].y,y,bloques[i].imagePath);
-                // }
 
                 if(bloques[i].x === x-1 && bloques[i].y === y-1 && bloques[i].imagePath!==undefined)
                 {
@@ -211,7 +157,11 @@ $(function() {
               }
             }
 
-            var cell = { x:x, y:y , type:type };
+            function blockOnClick(){
+              console.log("rect");
+            }
+
+            var cell = { x:x, y:y , type:type ,onClick:blockOnClick};
             /*dentro del objeto map en el atributo grid que es un arreglo bidimencional se guardara el objeto cell correspondiente*/
             map.grid[x][y] = cell;
             /*De acuerdo al tipo de la celda, se guardara en el atributo correspondiente dentro del objeto map la celda por medio 
@@ -221,18 +171,6 @@ $(function() {
     }
     /*Al finalizar la insercion y generacion de todas las celdas se regresara el objeto mapa*/
     return map;
-    //******************************************************************************************************
-    //***CONCLUSION*****************************************************************************************
-    //******************************************************************************************************
-    /*El objeto retornado map, contiene 4 atributos, todos se llenan con objetos cell, los objetos cell estan
-    conformados por medio de coordenadas "x" y "y" asi como el tipo correspondiente a la celda
-
-    Los 4 atributos que tiene el objeto retornado son:
-      - grid: este arreglo 2D guarda todas las celdas de manera ordenada con sus coordenadas y su tipo
-      - grass: este arreglo 1D guarda todas las celdas tipo grass
-      - rock: este arreglo 1D guarda todas las celdas tipo rock
-      - lava: este arreglo 1D guarda todas las celdas tipo lava*/
-
   }
 
 
@@ -255,8 +193,6 @@ $(function() {
     return { x:xScale, y:yScale };
   }
 
-
-
   function drawCells(svgContainer, scales, data, cssClass) {
     
     var gridGroup = svgContainer.append("g");
@@ -269,44 +205,11 @@ $(function() {
              .attr("y", function (d) { return scales.y(d.y); })
              .attr("width", function (d) { return squareLength; })
              .attr("height", function (d) { return squareLength; })
-             .attr("class", cssClass);
-  }
+             .attr("class", cssClass)
+             .on("click",function(){console.log("click",this);})
+             .on("mouseover",function(){d3.select(this).attr("class","mouseovered");})
+             .on("mouseout",function(){d3.select(this).attr("class",cssClass)});
 
-  function drawMowerHistory(groups, scales, path) {
-    // path
-    console.log("EL PATH VALE: "+path);
-    $("#terminal").val("EL PATH VALE: "+path[0].x+" "+path[0].y);
-    groups.path.selectAll(".path").remove();
-    var lineFunction = d3.svg.line()
-               .x(function(d) { return scales.x(d.x + 0.5); })
-               .y(function(d) { return scales.y(d.y + 0.5); })
-               .interpolate("linear");
-
-    var lineGraph = groups.path.append("path")
-                              .attr("d", lineFunction(path))
-                              .attr("class", "path")
-                              .attr("fill", "none");
-
-    // position
-    var circleData = groups.position.selectAll("circle").data(path);
-    circleData.exit().remove();
-    var circles = circleData.enter().append("circle");
-    var circleAttributes = circles
-             .attr("cx", function (d) { return scales.x(d.x + 0.5); })
-             .attr("cy", function (d) { return scales.y(d.y + 0.5); })
-             .attr("r", function (d) { return circleRadius; })
-             .attr("class", "position");
-
-    // position number
-    var textData = groups.position.selectAll("text").data(path);
-    textData.exit().remove();
-    var texts = textData.enter().append("text");
-    var textAttributes = texts
-             .attr("x", function (d) { return scales.x(d.x + 0.5); })
-             .attr("y", function (d) { return scales.y(d.y + 0.5); })
-             .attr("dy", ".31em")
-             .text(function(d,i) { return i; })
-             .attr("class", "positionNumber");
   }
 
   function drawMowerHistory2(groups, scales, path) {
@@ -326,6 +229,7 @@ $(function() {
         .attr("cy",function(d){return scales.y(d.y+0.5)})
         .attr("r",function(d){return circleRadius})
         .attr("class",function(d){return "position"});
+        
 
     groups.position.selectAll("circle")
         .data([path])
@@ -336,29 +240,6 @@ $(function() {
     var grass = map.grass;
     var i = Math.ceil(Math.random() * grass.length);
     return grass[i];
-  }
-
-  function getNext(map, current, command) {
-
-    var newX = 0;
-    var newVal = 0;
-
-    switch(command) {
-      case "U":
-        newVal = current.y-1<0 ? current.y:current.y-1;
-        return map.grid[current.x][newVal];
-      case "D":
-        newVal = current.y+1>map.grid[current.x].length ? current.y:current.y+1;
-        return map.grid[current.x][newVal];
-      case "R":
-        newVal = current.x+1>map.grid.length ? current.x:current.x+1;
-        return map.grid[newVal][current.y];
-      case "L":
-        newVal = current.x-1<0 ? current.x:current.x-1;
-        return map.grid[newVal][current.y];
-      default:
-        throw "Unexpected command : "+command;
-      }
   }
 
   function getNext2(map, newPos) {
@@ -375,35 +256,6 @@ $(function() {
 
   var path = null;
 
-  function executeCommands(e) {
-    var content = $('#commands').val();
-    content = content.toUpperCase().replace(/[^UDRL]/g, "");
-    $('#commands').val(content);
-    path = path!=null ? path:[start];
-    var current = start;
-    for(i = 0; i < content.length; i++) {
-      var next = getNext(map, current, content[i]);
-      switch(next.type) {
-        case "grass":
-          path.push(next);
-          current = next;
-          break;
-        case "rock":
-          // stay at the same place
-          break;
-        case "lava":
-          drawMowerHistory(groups, scales, path);
-          alert("The mower turned into ashes, as predicted.", "Start again.");
-          $('#commands').val("");
-          drawMowerHistory(groups, scales, [start]);
-          return;
-        default:
-          throw "Unexpected terrain type "+next.type;
-      }
-    }
-    drawMowerHistory2(groups, scales, path);
-  }
-
   function executeCommands2(e)
   {
     var content = $('#commands').val();
@@ -419,11 +271,9 @@ $(function() {
     }
   }
 
-
-
   var squareLength = 15;
   var circleRadius = 15;
-  var ratios = { rock:0.05, lava:0.05 };
+  var ratios = { rock:0.05, border:0.05 };
 
   var layoutSize = {x:94 ,y: 80};
 
@@ -476,7 +326,7 @@ $(function() {
 
     drawCells(svgContainer, scales, map.grass, "grass");
     drawCells(svgContainer, scales, map.rock, "rock");
-    drawCells(svgContainer, scales, map.lava, "lava");
+    drawCells(svgContainer, scales, map.border, "border");
 
     groups = { path:svgContainer.append("g"),
                     position:svgContainer.append("g") };
@@ -486,30 +336,13 @@ $(function() {
 
   initEverything();
 
-  $('#commands').on('input', executeCommands2);
-
-  /*$(function (){
-    var socket = io();
-    
-
-    socket.on('chat message',function(msg){
-        var valorActual = $('#commands').val();
-        $('#commands').val(valorActual+msg);
-        console.log("LLEGO UN MENSAJE")
-        executeCommands2();
-      });
-
-      return false;
-    });*/
-
   $(function (){
     var socket = io();
-    
-
     socket.on('chat message',function(msg){
         try
         {
           var json = JSON.parse(msg);
+          console.log(msg);
           var next = getNext2(map,json)
           if(next !== null)
           {
